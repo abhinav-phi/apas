0b7bd3aa29de67404d6296c5a6201d37dd7d408d
abhinav-phi <alpha9coder@gmail.com>
2026-08-20 23:47:18 +0530

    chore(types): add env typings for blockchain vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
+  readonly VITE_APP_NAME?: string;
+  readonly VITE_APP_URL?: string;
+  readonly VITE_SEPOLIA_RPC_URL?: string;
+  readonly VITE_SEPOLIA_RPC_FALLBACK_1?: string;
+  readonly VITE_SEPOLIA_RPC_FALLBACK_2?: string;
+  readonly VITE_CONTRACT_ADDRESS?: string;
+  readonly VITE_CHAIN_ID?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
